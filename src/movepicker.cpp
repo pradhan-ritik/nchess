64494ba95fe5ec304da27b882caf203a93a4b12e
rp <pradhanr@mailfence.com>
2025-03-25 18:32:21 -0700

    fixed quiet move generation

--- src/movepicker.cpp
+++ src/movepicker.cpp
@@ -26,19 +26,19 @@ Move Movepicker::next_move() {
             }
 
         case GENERATE_QUIET:
-        if (movelist_length == 0) { // need to generate moves
-            movelist_length = generate_moves<GENERATE_QUIET>(*board, movelist);
-            movelist.reset_index();
-        }
+            if (movelist_length == 0) { // need to generate moves
+                movelist_length = generate_moves<GENERATE_QUIET>(*board, movelist);
+                movelist.reset_index();
+            }
 
-        if (movelist.plus_plus_index() == movelist_length) {
-            stage++;
-            movelist_length = 0;
-        }
+            if (movelist.plus_plus_index() == movelist_length) {
+                stage++;
+                movelist_length = 0;
+            }
 
-        else {
-            return movelist.get_move_at(movelist.get_index());
-        }
+            else {
+                return movelist.get_move_at(movelist.get_index());
+            }
 
         case MOVEGEN_FINISHED:
             return NULL_MOVE;
