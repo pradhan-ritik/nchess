6a542a01f0dd1ad7217a752f0e41d34b45b6a157
pradhan-ritik <pradhanr@mailfence.com>
2025-03-30 13:41:14 -0700

    Fixed square_attacks

--- src/board.hpp
+++ src/board.hpp
@@ -46,7 +46,7 @@ struct Board {
 
     public:
     Board(const char* fen);
-    void set_game(const char* fen);
+    void set_game(const char* fen, bool from_interface=true);
     void display_game(bool show_bitboards=false);
     bool play_move(Move move); // returns true if the move was legal
     void undo_last_move();
@@ -193,7 +193,7 @@ struct Board {
         BB other_team_queens = get_piece_bb(QUEEN, other_team);
         return ((other_team_queens | get_piece_bb(BISHOP, other_team)) & bishop_attacks(pos, empties)) |
                 ((other_team_queens | get_piece_bb(ROOK, other_team)) & rook_attacks(pos, empties)) |
-                (pawn_attacks(get_piece_bb(PAWN, pos), same_team) & get_piece_bb(PAWN, other_team)) |
+                (get_piece_bb(PAWN, other_team) & pawn_attacks(pos, !other_team)) |
                 (knight_attacks(pos) & get_piece_bb(KNIGHT, other_team))
                 ;
     }
