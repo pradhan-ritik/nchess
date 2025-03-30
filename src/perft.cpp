993fb7d0d4c35868bec6503b8da6ddc6a88b0bc1
pradhan-ritik <pradhanr@mailfence.com>
2025-03-30 13:44:23 -0700

    Removed bulk counting

--- src/perft.cpp
+++ src/perft.cpp
@@ -11,14 +11,15 @@ BB perft(Board& board, int depth) {
     Move move;
     while ((move = mp.next_move()) != NULL_MOVE) {
         // printf("here %s\n", move_to_uci(move));
-        if (depth != 1) {
-            board.play_move(move);
-            if (board.was_move_legal()) {
-                nodes += perft(board, depth - 1);
-            }
-            board.undo_last_move();
+        // if (depth != 1) {
+        board.play_move(move);
+        if (board.was_move_legal()) {
+            nodes += perft(board, depth - 1);
         }
-        else nodes++;
+
+        board.undo_last_move();
+         //}
+       //  else nodes++;
     }
 
     return nodes;
@@ -37,14 +38,15 @@ BB perft_divide(Board& board, int depth) {
     while ((move = mp.next_move()) != NULL_MOVE) {
         board.play_move(move);
         if (board.was_move_legal()) {
-            if (depth == 1)
-                nodes_here = 1;
-            else
-                nodes_here = perft(board, depth - 1);
+            //if (depth == 1)
+             //   nodes_here = 1;
+            //else
+              nodes_here = perft(board, depth - 1);
         
             nodes += nodes_here;
             printf("%s: %llu\n", move_to_uci(move), nodes_here);
         }
+
         board.undo_last_move();
     }
 
@@ -52,5 +54,5 @@ BB perft_divide(Board& board, int depth) {
 }
 
 BB perft_info(Board& board, int depth) {
-    printf("Nodes searched: %llu\n", perft_divide(board, depth));
+    printf("\nNodes searched: %llu\n", perft_divide(board, depth));
 }
